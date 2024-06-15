4a929359e4d4461053a259918a5df774f0a6f717
da-roth <118249021+da-roth@users.noreply.github.com>
2024-06-15 11:19:47 +0200

    some cleaning

--- DifferentiationInterface/src/diffipy/__init__.py
+++ DifferentiationInterface/src/diffipy/__init__.py
@@ -1,11 +1,6 @@
 from .Node import *
 from .NodesOperations import *
-
-# from .NodesPytorch import *
 from .NodesVariables import *
-# from .NodesNumpy import *
-# from .NodesTensorflow import *
-# from .NodesJax import *
 
 from .backend_config import *
 from .diffipy import *
